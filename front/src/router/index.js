8d5f662e3d248346489c0ef94dff763ff0e65489
icon <filaos974@hotmail.com>
2022-08-09 16:09:35 +0400

    manque fichier

---
+++ front/src/router/index.js
@@ -0,0 +1,50 @@
+// gère les routes(pages)
+import {createRouter, createWebHistory} from 'vue-router'
+import Accueil from '@/views/Accueil.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'Accueil',
+    component: Accueil,
+    alias: '/index.html'
+  },
+  {
+    path: '/Page1',
+    name: 'Page1',
+    component: () => import(/* webpackChunkName: "Artist" */ '../views/Page1.vue')
+  },
+  {
+    path: '/Page2',
+    name: 'Page2',
+    component: () => import(/* webpackChunkName: "Artist" */ '../views/Page2.vue')
+  },
+  {
+    path: '/Page3',
+    name: 'Page3',
+    component: () => import(/* webpackChunkName: "Artist" */ '../views/Page3.vue')
+  },
+  {
+    path: '/Page4',
+    name: 'Page4',
+    component: () => import(/* webpackChunkName: "Artist" */ '../views/Page4.vue')
+  },
+  {
+    path: '/Page5',
+    name: 'Page5',
+    component: () => import(/* webpackChunkName: "Artist" */ '../views/Page5.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return {
+      top: 0,
+      behavior: 'smooth'
+    }
+  }
+})
+
+export default router
