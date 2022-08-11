1bc12dc5bcc40fd792fbc3c7b6e06c56271d792d
icon <filaos974@hotmail.com>
2022-08-11 16:26:32 +0400

    modifification accueil et css

--- front/src/views/Accueil.vue
+++ front/src/views/Accueil.vue
@@ -1,61 +1,76 @@
 <template>
- <div>{{ resultat }}</div>
- <!-- 
+  <div>{{ resultat }}</div>
+  <!-- 
  	<head>
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	</head>
   -->
- 
-  <section id="intro" class="main style1 dark fullscreen">
+
+  <section id="intro" class="main style1 dark fullscreen" :style="{ backgroundImage: `url(${backgroundImage})` }">
     <div>
       <header>
         <h2 style="color:white;">Jardin App.</h2>
       </header>
-      <p style="color:white;">Bienvenue à  <strong>App name ou Autre!</strong> Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
-        Fusce blandit ultrices sapien, in accumsan orci rhoncus eu! </p>
-      <br/><br/>
-      <ScanQrcode :expected="expected" :qrbox="250" :fps="10" style="width: 400px;height: 400px;" @resultat="onScan" />
-      
+      <p style="color:white;">
+        Bienvenue à
+        <strong>App name ou Autre!</strong> Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+        Fusce blandit ultrices sapien, in accumsan orci rhoncus eu!
+      </p>
+      <br />
+      <br />
+      <ScanQrcode
+        :expected="expected"
+        :qrbox="250"
+        :fps="10"
+        style="width: 400px;height: 400px;"
+        @resultat="onScan"
+      />
+
       <footer>
         <a href="#one" class="button style2 down">More</a>
       </footer>
-		</div>
+    </div>
   </section>
 
   <section id="one" class="main style2 right dark fullscreen">
     <div class="content box style2">
       <header>
-        <h2> Les compositions</h2>
+        <h2>Les compositions</h2>
       </header>
       <p>
         Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
         Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis arcu,
-        id varius justo euismod in. Curabitur egestas consectetur magna.<br/>
-        ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+        id varius justo euismod in. Curabitur egestas consectetur magna.
+        <br />ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
         Fusce blandit ultrices sapien, Sed sodales venenatis arcu,
-        id varius justo euismod in. Curabitur egestas consectetur magna.<br/>
+        id varius justo euismod in. Curabitur egestas consectetur magna.
+        <br />
       </p>
     </div>
-	</section>
-
+  </section>
 </template>
 
 <script setup>
 import ScanQrcode from '@/components/ScanQrcode.vue'
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
+
 // routes
-import {useRouter} from 'vue-router'
+import { useRouter } from 'vue-router'
+
+// medias: images en background
+import backgroundImage from "@/assets/images/first.jpg"
 
 const router = useRouter()
 
 const routesQrCode = [
-  {code: "https://m.tibillet.re/q474d", route: "/Page5"},
-  {code: "https://m.tibillet.re/sd4df", route: "/Page4"},
-  {code: "https://m.tibillet.re/q47f7", route: "/Page3"},
-  {code: "https://m.tibillet.re/q47s4", route: "/Page2"},
-  {code: "https://m.tibillet.re/q474s", route: "/Page1"}
+  { code: "https://m.tibillet.re/q474d", route: "/Page5" },
+  { code: "https://m.tibillet.re/sd4df", route: "/Page4" },
+  { code: "https://m.tibillet.re/q47f7", route: "/Page3" },
+  { code: "https://m.tibillet.re/q47s4", route: "/Page2" },
+  { code: "https://m.tibillet.re/q474s", route: "/Page1" }
 ]
+
 
 // résultats qrcodes attendu
 let expected = []
@@ -89,10 +104,16 @@ onMounted(() => {
   // élimine l'élément #reader
   //   const ele = document.querySelector('#reader')
   //   ele.parentNode.removeChild(ele)
-    console.log("-> test =", document.querySelector('#reader'))
+  console.log("-> test =", document.querySelector('#reader'))
 })
 </script>
 
 <style>
-
+#intro {
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
