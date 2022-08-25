3541fe42e5e700e585dfcdc19632913fe42722ef
Glen <glen84_dr@hotmail.com>
2022-08-25 22:40:22 +0400

    images directing to pages

--- front/src/views/Accueil.vue
+++ front/src/views/Accueil.vue
@@ -1,31 +1,39 @@
 <template>
-  <div>{{ resultat }}</div>
-
   <section id="intro" :style="{ backgroundImage: `url(${backgroundImage})` }">
+    <!-- <buton class="text-primary" @click="router.push('/PageTest')">TestPage</buton> -->
     <div>
       <header>
         <h2 style="color:white;">Jardin App.</h2>
       </header>
       <p style="color:white;">
         Bienvenue à
-        <strong>App name ou Autre!</strong> Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+        <strong>App name ou Autre!</strong> Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore
+        condimentum.
         Fusce blandit ultrices sapien, in accumsan orci rhoncus eu!
       </p>
-      <br />
-      <br />
 
-      <ScanQrcode :expected="expected" :qrbox="250" :fps="10" style="width: 400px;height: 400px;" @resultat="onScan" />
-      <br/><br/><br/><br/><br/><br/>
-      
-      <footer>
+      <ScanQrcode :expected="expected" :qrbox="250" :fps="10" style="width: 400px;height: 400px;" @resultat="onScan"/>
+
+      <div class="d-flex flex-row justify-content-sm-center align-items-center mt-2">
+        <img :src="oiseau" class="ms-1 me-1" role="button" @click="router.push('/Page1')">
+        <img :src="oiseau" class="ms-1 me-1" role="button" @click="router.push('/Page2')">
+        <img :src="oiseau" class="ms-1 me-1" role="button" @click="router.push('/Page3')">
+        <img :src="oiseau" class="ms-1 me-1" role="button" @click="router.push('/Page4')">
+        <img :src="oiseau" class="ms-1 me-1" role="button" @click="router.push('/Page5')">
+      </div>
+
+      <footer class="mt-5">
         <a href="#one">
-          <button type="button" class="btn bg-transparent" style='font-size: 25px; border-color:white; color:white;'>&dArr;</button>
+          <button type="button" class="btn bg-transparent" style='font-size: 25px; border-color:white; color:white;'>
+            <BIconArrowDown/>
+          </button>
         </a>
       </footer>
     </div>
   </section>
 
-  <section id="one" class="main style2 right dark fullscreen" :style="{ backgroundImage: `url(${backgroundImageOne})` }">
+  <section id="one" class="main style2 right dark fullscreen"
+           :style="{ backgroundImage: `url(${backgroundImageOne})` }">
 
     <div class="content-style2">
       <div class="slide-right">
@@ -41,10 +49,10 @@
           Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
           Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis arcu,
           id varius justo euismod in. Curabitur egestas consectetur magna.
-          <br />ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+          <br/>ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
           Fusce blandit ultrices sapien, Sed sodales venenatis arcu,
           id varius justo euismod in. Curabitur egestas consectetur magna.
-          <br />
+          <br/>
         </p>
       </div>
     </div>
@@ -54,44 +62,34 @@
 
 <script setup>
 import ScanQrcode from '@/components/ScanQrcode.vue'
-import { ref } from 'vue'
-
+import {ref} from 'vue'
 // routes
-import { useRouter } from 'vue-router'
-
+import {useRouter} from 'vue-router'
 // medias: images en background
 import backgroundImage from "@/assets/images/first.jpg"
 import backgroundImageOne from "@/assets/images/one.jpg"
-
+import oiseau from "@/assets/images/oiseau_40x40.png"
+// icon
+import {BIconArrowDown} from 'bootstrap-icons-vue'
 const router = useRouter()
-
 const routesQrCode = [
-  { code: "https://m.tibillet.re/q474d", route: "/Page5" },
-  { code: "https://m.tibillet.re/sd4df", route: "/Page4" },
-  { code: "https://m.tibillet.re/q47f7", route: "/Page3" },
-  { code: "https://m.tibillet.re/q47s4", route: "/Page2" },
-  { code: "https://m.tibillet.re/q474s", route: "/Page1" }
+  {code: "chpt", route: "/PageTest"},
+  {code: "chp5", route: "/Page5"},
+  {code: "chp4", route: "/Page4"},
+  {code: "chp3", route: "/Page3"},
+  {code: "chp2", route: "/Page2"},
+  {code: "chp1", route: "/Page1"}
 ]
- // https://raffinerie.tibillet.re/qr/07510c96-6eda-48a9-b31e-149042068112
-
-
+// https://raffinerie.tibillet.re/qr/07510c96-6eda-48a9-b31e-149042068112
 // résultats qrcodes attendu
 let expected = []
 for (let i = 0; i < routesQrCode.length; i++) {
   expected.push(routesQrCode[i].code)
 }
-
-
-let resultat = ref('')
-
-function onScan(decodedText, decodedResult) {
+function onScan(qrCodeMessage) {
   console.log('-> fonc onScan !')
-  console.log('decodedText =', decodedText)
-  console.log('decodedResult =', decodedResult)
-  resultat.value = decodedText
-
-
-  const test = routesQrCode.find(obj => obj.code === decodedText)
+  console.log('qrCodeMessage =', qrCodeMessage)
+  const test = routesQrCode.find(obj => obj.code === qrCodeMessage)
   if (test !== undefined) {
     // Stoper le  lecteur de qrcode
     // Aller à la page
@@ -102,7 +100,6 @@ function onScan(decodedText, decodedResult) {
   }
   console.log('test =', test)
 }
-
 </script>
 
 <style>
@@ -113,9 +110,7 @@ function onScan(decodedText, decodedResult) {
   background-repeat: no-repeat;
   background-size: cover;
 }
-
 /*    2iem page accueil! */
-
 #one {
   background-size: cover;
   width: 100vw;
@@ -124,7 +119,6 @@ function onScan(decodedText, decodedResult) {
   background-attachment: fixed, fixed;
   background-position: top left, center center;
 }
-
 .content-style2 {
   margin: auto;
   width: 60%;
@@ -132,19 +126,16 @@ function onScan(decodedText, decodedResult) {
   padding: 10%;
   overflow: hidden;
   box-shadow: 0 0 25px #000;
-  border: 1px solid #444;  
+  border: 1px solid #444;
   text-align: center;
 }
-
-p{
+p {
   font-size: 20px;
   color: #444;
 }
-
 .slide-right, .slide-left {
   width: 100%;
 }
-
 .slide-right {
   animation: 3s slide-right;
 }
@@ -156,7 +147,6 @@ p{
     margin-left: 0%;
   }
 }
-
 /***** Slide Left *****/
 .slide-left {
   animation: 3s slide-left;
@@ -169,7 +159,6 @@ p{
     margin-left: 0%;
   }
 }
-
 .line {
   border-bottom: 3px solid red;
   width: 200px;
@@ -178,13 +167,12 @@ p{
 .fade-in {
   animation: fadeIn ease 3s;
 }
-@keyframes fadeIn{
+@keyframes fadeIn {
   0% {
-    opacity:0;
+    opacity: 0;
   }
   100% {
-    opacity:1;
+    opacity: 1;
   }
 }
-
 </style>
